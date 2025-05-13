bb738413f83f86bac6b081bd28a981465429847c
jeetesh maurya <123884309+jeetxgeek@users.noreply.github.com>
2025-05-13 20:07:46 +0000

    new files added

---
+++ Basics01/Datatypes.js
@@ -0,0 +1,10 @@
+"use strict" //treat all JS code as Newer JS or newer version
+
+//alert(3+3) //we r using node.js not browser console
+
+console.log(3+30)
+
+
+
+
+
---
+++ Basics01/variables.js
@@ -0,0 +1,14 @@
+const acctID = 12236 //non changeable value
+let email = 'jeetesh57@google.com'
+var acctPass= "112233"
+acctcity = 'jaipur'
+let acctstate;
+
+
+acctcity = "California"
+email = "hhhh@jsnsn.com"
+acctPass = 'bdbcbwkbd'
+
+console.table([acctcity, email, acctPass, acctstate])
+
+/* prefer not to use var because block scope and functional scope */
